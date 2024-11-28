revised wrong order of input decoded, x -> x, decoded.

src/layers/autoencoder/vae.py
@@ -127,7 +127,7 @@ class OobleckDecoder(nn.Module):
     def __init__(self, 
                  out_channels=1, 
                  channels=128, 
-                 latent_dim=128, 
+                 latent_dim=64, 
                  c_mults = [1, 2, 4, 8, 16], 
                  strides = [2, 4, 8, 8, 8],):
                
@@ -138,6 +138,7 @@ class OobleckDecoder(nn.Module):
         self.depth = len(c_mults)
 
         layers = [
+            get_activation_fn("snake", in_chn=latent_dim),
             nn.Conv1d(in_channels=latent_dim, out_channels=c_mults[-1]*channels, kernel_size=7, padding=3),
         ]
         
@@ -188,6 +189,7 @@ class VAEBottleneck(nn.Module):
 
     def decode(self,x):
         return x
+    
 class AEBottleneck(nn.Module):
     def __init__(self):
         super().__init__()
@@ -212,7 +214,7 @@ class AudioAutoencoder(nn.Module):
         downsampling_ratio=2048,
         sample_rate=8000,
         io_channels=1,
-        bottleneck = AEBottleneck(),
+        bottleneck = VAEBottleneck(),
         in_channels = 1,
         out_channels = 1,
     ):

src/layers/tools/losses.py
@@ -5,12 +5,33 @@ from typing import List, Any
 import numpy as np
 # https://github.com/csteinmetz1/auraloss/tree/main
 # paper suggests L1 Loss & MR is the best performing one..
-
+import scipy
 class Loss(nn.Module):
     def __init__(self,*args,**kwargs):
         super().__init__()
         
     
+
+def get_window(win_type: str, win_length: int):
+    """Return a window function.
+
+    Args:
+        win_type (str): Window type. Can either be one of the window function provided in PyTorch
+            ['hann_window', 'bartlett_window', 'blackman_window', 'hamming_window', 'kaiser_window']
+            or any of the windows provided by [SciPy](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.windows.get_window.html).
+        win_length (int): Window length
+
+    Returns:
+        win: The window as a 1D torch tensor
+    """
+
+    try:
+        win = getattr(torch, win_type)(win_length)
+    except:
+        win = torch.from_numpy(scipy.signal.windows.get_window(win_type, win_length))
+
+    return win
+
 class LossModule:
     def __init__(self, name):
         self.name = name
@@ -44,7 +65,6 @@ class LossModule:
             total_loss += l
             info.update({name : l})
         
-        info.update({"total loss": total_loss})
         
         return total_loss, info
     
@@ -53,7 +73,7 @@ class L1Loss(Loss):
         super().__init__()
         self.loss = nn.L1Loss(reduction=distance)
     def forward(self,x,y):
-        return self.loss(x, y)
+        return self.loss(y, x)
 
 class SpectralConvergenceLoss(Loss):
     """Spectral convergence loss module.
@@ -109,7 +129,16 @@ class STFTMagnitudeLoss(Loss):
             y_mag = torch.log(self.log_fac * y_mag + self.log_eps)
         return self.distance(x_mag, y_mag)
 
-class STFTLoss(Loss):
+
+def apply_reduction(losses, reduction="none"):
+    """Apply reduction to collection of losses."""
+    if reduction == "mean":
+        losses = losses.mean()
+    elif reduction == "sum":
+        losses = losses.sum()
+    return losses
+
+class STFTLoss(torch.nn.Module):
     """STFT loss module.
 
     See [Yamamoto et al. 2019](https://arxiv.org/abs/1904.04472).
@@ -131,6 +160,7 @@ class STFTLoss(Loss):
             ['mel', 'chroma']
             Default: None
         n_bins (int, optional): Number of scaling frequency bins. Default: None.
+        perceptual_weighting (bool, optional): Apply perceptual A-weighting (Sample rate must be supplied). Default: False
         scale_invariance (bool, optional): Perform an optimal scaling of the target. Default: False
         eps (float, optional): Small epsilon value for stablity. Default: 1e-8
         output (str, optional): Format of the loss returned.
@@ -157,13 +187,15 @@ class STFTLoss(Loss):
         fft_size: int = 1024,
         hop_size: int = 256,
         win_length: int = 1024,
+        window: str = "hann_window",
         w_sc: float = 1.0,
         w_log_mag: float = 1.0,
         w_lin_mag: float = 0.0,
         w_phs: float = 0.0,
         sample_rate: float = None,
-        scale: str = "chroma",
+        scale: str = None,
         n_bins: int = None,
+        perceptual_weighting: bool = False,
         scale_invariance: bool = False,
         eps: float = 1e-8,
         output: str = "loss",
@@ -176,7 +208,7 @@ class STFTLoss(Loss):
         self.fft_size = fft_size
         self.hop_size = hop_size
         self.win_length = win_length
-        self.window = torch.hann_window(win_length)
+        self.window = get_window(window, win_length)
         self.w_sc = w_sc
         self.w_log_mag = w_log_mag
         self.w_lin_mag = w_lin_mag
@@ -184,12 +216,14 @@ class STFTLoss(Loss):
         self.sample_rate = sample_rate
         self.scale = scale
         self.n_bins = n_bins
+        self.perceptual_weighting = perceptual_weighting
         self.scale_invariance = scale_invariance
         self.eps = eps
         self.output = output
         self.reduction = reduction
         self.mag_distance = mag_distance
         self.device = device
+
         self.phs_used = bool(self.w_phs)
 
         self.spectralconv = SpectralConvergenceLoss()
@@ -216,31 +250,34 @@ class STFTLoss(Loss):
 
             if self.scale == "mel":
                 assert sample_rate != None  # Must set sample rate to use mel scale
-                assert 32 <= fft_size  # Must be more FFT bins than Mel bins
-                fb = librosa.filters.mel(sr=sample_rate, n_fft=fft_size, n_mels=32)
+                assert n_bins <= fft_size  # Must be more FFT bins than Mel bins
+                fb = librosa.filters.mel(sr=sample_rate, n_fft=fft_size, n_mels=n_bins)
                 fb = torch.tensor(fb).unsqueeze(0)
 
             elif self.scale == "chroma":
                 assert sample_rate != None  # Must set sample rate to use chroma scale
-                assert 32 <= fft_size  # Must be more FFT bins than chroma bins
+                assert n_bins <= fft_size  # Must be more FFT bins than chroma bins
                 fb = librosa.filters.chroma(
-                    sr=sample_rate, n_fft=fft_size, n_chroma=32
+                    sr=sample_rate, n_fft=fft_size, n_chroma=n_bins
                 )
-                torch.tensor(fb).unsqueeze(0)
 
             else:
                 raise ValueError(
                     f"Invalid scale: {self.scale}. Must be 'mel' or 'chroma'."
                 )
 
-       
-            self.register_buffer("fb", torch.tensor(fb))
+            self.register_buffer("fb", fb)
 
         if scale is not None and device is not None:
             self.fb = self.fb.to(self.device)  # move filterbank to device
-        self.prefilter = FIRFilter(filter_type="aw", fs=sample_rate)
-        
-    
+
+        if self.perceptual_weighting:
+            if sample_rate is None:
+                raise ValueError(
+                    f"`sample_rate` must be supplied when `perceptual_weighting = True`."
+                )
+            self.prefilter = FIRFilter(filter_type="aw", fs=sample_rate)
+
     def stft(self, x):
         """Perform STFT.
         Args:
@@ -271,21 +308,21 @@ class STFTLoss(Loss):
         return x_mag, x_phs
 
     def forward(self, input: torch.Tensor, target: torch.Tensor):
-        
-        bs, seq_len = input.size()
-        chs = 1
-        # since FIRFilter only support mono audio we will move channels to batch dim
-        input = input.view(bs * chs, 1, -1)
-        target = target.view(bs * chs, 1, -1)
+        bs, chs, seq_len = input.size()
 
-        # now apply the filter to both
-        self.prefilter.to(input.device)
-        input, target = self.prefilter(input, target)
+        if self.perceptual_weighting:  # apply optional A-weighting via FIR filter
+            # since FIRFilter only support mono audio we will move channels to batch dim
+            input = input.view(bs * chs, 1, -1)
+            target = target.view(bs * chs, 1, -1)
 
-        # now move the channels back
-        input = input.view(bs, chs, -1)
-        target = target.view(bs, chs, -1)
-        
+            # now apply the filter to both
+            self.prefilter.to(input.device)
+            input, target = self.prefilter(input, target)
+
+            # now move the channels back
+            input = input.view(bs, chs, -1)
+            target = target.view(bs, chs, -1)
+
         # compute the magnitude and phase spectra of input and target
         self.window = self.window.to(input.device)
 
@@ -317,14 +354,16 @@ class STFTLoss(Loss):
             + (self.w_phs * phs_loss)
         )
 
-        loss = loss.mean()
+        loss = apply_reduction(loss, reduction=self.reduction)
 
         if self.output == "loss":
             return loss
         elif self.output == "full":
             return loss, sc_mag_loss, log_mag_loss, lin_mag_loss, phs_loss
 
-class MultiResolutionSTFTLoss(Loss):
+
+
+class MultiResolutionSTFTLoss(torch.nn.Module):
     """Multi resolution STFT loss module.
 
     See [Yamamoto et al., 2019](https://arxiv.org/abs/1910.11480)
@@ -343,22 +382,17 @@ class MultiResolutionSTFTLoss(Loss):
         sample_rate (int, optional): Sample rate. Required when scale = 'mel'. Default: None
         scale (str, optional): Optional frequency scaling method, options include:
             ['mel', 'chroma']
-            Default: 'chroma'
+            Default: None
         n_bins (int, optional): Number of mel frequency bins. Required when scale = 'mel'. Default: None.
         scale_invariance (bool, optional): Perform an optimal scaling of the target. Default: False
     """
-    '''
-    "fft_sizes": [2048, 1024, 512, 256, 128, 64, 32],
-                    "hop_sizes": [512, 256, 128, 64, 32, 16, 8],
-                    "win_lengths": [2048, 1024, 512, 256, 128, 64, 32],
-                    "perceptual_weighting": true
-    '''
 
     def __init__(
         self,
-        fft_sizes: List[int] = [2048, 1024, 512, 256, 128, 64, 32],
-        hop_sizes: List[int] = [512, 256, 128, 64, 32, 16, 8],
-        win_lengths: List[int] = [2048, 1024, 512, 256, 128, 64, 32],
+        fft_sizes: List[int] = [1024, 2048, 512],
+        hop_sizes: List[int] = [120, 240, 50],
+        win_lengths: List[int] = [600, 1200, 240],
+        window: str = "hann_window",
         w_sc: float = 1.0,
         w_log_mag: float = 1.0,
         w_lin_mag: float = 0.0,
@@ -366,6 +400,7 @@ class MultiResolutionSTFTLoss(Loss):
         sample_rate: float = None,
         scale: str = None,
         n_bins: int = None,
+        perceptual_weighting: bool = False,
         scale_invariance: bool = False,
         **kwargs,
     ):
@@ -382,6 +417,7 @@ class MultiResolutionSTFTLoss(Loss):
                     fs,
                     ss,
                     wl,
+                    window,
                     w_sc,
                     w_log_mag,
                     w_lin_mag,
@@ -389,13 +425,13 @@ class MultiResolutionSTFTLoss(Loss):
                     sample_rate,
                     scale,
                     n_bins,
+                    perceptual_weighting,
                     scale_invariance,
                     **kwargs,
                 )
             ]
 
     def forward(self, x, y):
-        #https://static1.squarespace.com/static/5554d97de4b0ee3b50a3ad52/t/5fb1e9031c7089551a30c2e4/1605495044128/DMRN15__auraloss__Audio_focused_loss_functions_in_PyTorch.pdf
         mrstft_loss = 0.0
         sc_mag_loss, log_mag_loss, lin_mag_loss, phs_loss = [], [], [], []
 
@@ -420,6 +456,7 @@ class MultiResolutionSTFTLoss(Loss):
 
 
 
+
 def get_hinge_losses(score_real, score_fake):
     gen_loss = -score_fake.mean()
     dis_loss = torch.relu(1 - score_real).mean() + torch.relu(1 + score_fake).mean()
@@ -438,7 +475,7 @@ class EncodecDiscriminator(nn.Module):
         logits, features = self.discriminators(x)
         return logits, features
 
-    def loss(self, y, x):
+    def loss(self, x, y):
         feature_matching_distance = 0.
         logits_true, feature_true = self.forward(x)
         logits_fake, feature_fake = self.forward(y)

src/train_autoencoder.py
@@ -2,7 +2,7 @@ import os
 import yaml
 import torch
 import torch.nn as nn
-from torch.optim import Adam
+from torch.optim import AdamW,Adam
 from layers.preprocess import load_mp3_files, create_overlapping_chunks_tensor
 from torch.utils.data import TensorDataset, DataLoader
 from datetime import datetime
@@ -28,39 +28,26 @@ class Trainer:
         ).to(self.MODEL_CFG['device'])
         
         self.loss = LossModule(name="AutoEncoder")
-        
-        # This one was the problemtic one. I need to train this crazy-ass Discriminator, not use it as a loss!!!!
-        #self.loss.append("discriminator", weight_loss=1.0, module=)
-        
-        self.loss.append("mrstft", weight_loss=3.0, module=MultiResolutionSTFTLoss(
+        self.loss.append("mrstft", weight_loss=0.25, module=MultiResolutionSTFTLoss(
             sample_rate=self.FFT_CFG['sr']).to(self.MODEL_CFG['device']))
-        #self.loss.append("L1", weight_loss=0.1,module=L1Loss().to(self.MODEL_CFG['device']))
-        
-        
-        '''Loader'''
-        self.train_loader, self.test_loader = self.getLoader()
-        
-        
-        warmup_period = self.MODEL_CFG['warmup_period']
-        num_steps = len(self.train_loader) * self.MODEL_CFG['epoch'] - warmup_period
+        self.loss.append("L1", weight_loss=0.1,module=L1Loss().to(self.MODEL_CFG['device']))        
 
+        
+        
+        
+        #warmup_period = self.MODEL_CFG['warmup_period']
         self.discriminator = EncodecDiscriminator().to(self.MODEL_CFG['device'])
-        self.optim_dis = Adam(self.discriminator.parameters(), lr=self.MODEL_CFG['lr'])
-        self.lr_schedule_dis = torch.optim.lr_scheduler.CosineAnnealingLR(self.optim_dis, T_max=num_steps)
-        self.warmup_schedule_dis = warmup.ExponentialWarmup(self.optim_dis, warmup_period)
-
-        # In __init__
-        self.optim_gen = Adam(self.net.parameters(), lr=self.MODEL_CFG['lr'])
-        self.lr_schedule_gen = torch.optim.lr_scheduler.CosineAnnealingLR(self.optim_gen, T_max=num_steps)
-        self.warmup_schedule_gen = warmup.ExponentialWarmup(self.optim_gen, warmup_period)
+        self.optim_dis = AdamW(self.discriminator.parameters(), lr=self.MODEL_CFG['lr'] * 2, betas=(0.8,0.99))
+        #self.lr_schedule_dis = torch.optim.lr_scheduler.CosineAnnealingLR(self.optim_dis, T_max=num_steps)
+        #self.warmup_schedule_dis = warmup.ExponentialWarmup(self.optim_dis, warmup_period)
+        self.optim_gen = AdamW(self.net.parameters(), lr=self.MODEL_CFG['lr'], betas=(0.8,0.99))
+        #self.lr_schedule_gen = torch.optim.lr_scheduler.CosineAnnealingLR(self.optim_gen, T_max=num_steps)
+        #self.warmup_schedule_gen = warmup.ExponentialWarmup(self.optim_gen, warmup_period)
         
         
         '''LOG'''
-        self.train_losses = []
-        self.eval_losses = []
-        self.best_eval_loss = float('inf')
-
-        
+        self.best_eval_loss = 1e+7
+        self.train_loader, self.test_loader = self.getLoader()
         wandb_store = os.path.join(self.FILE_CFG['log_dir'],"wandb_store")
         os.makedirs(wandb_store,exist_ok=True)
        
@@ -90,86 +77,76 @@ class Trainer:
 
         for epoch in range(self.MODEL_CFG['epoch']):
             
-            EPOCH_LOSS = []
+            epochloss = []
 
             for i, x in enumerate(tqdm(self.train_loader, desc=f"Epoch {epoch+1}/{EPOCH}")):
-                
-                
-                x = x[0].to(self.MODEL_CFG['device']).squeeze(1)
+                info = {}
+                x = x[0].to(self.MODEL_CFG['device']).unsqueeze(1)  # batch, 1, seq
                 latents = self.net.encode(x)
-                decoded = self.net.decode(latents)
+                decoded = self.net.decode(latents).unsqueeze(1) # batch, 1, seq
 
                 
+                dis_loss, adv_loss, feature_matching_loss = self.discriminator.loss(x, decoded)
                 
                 if (self.loss.discriminator_step % 2 == 0):
-                    '''
-                        note that discriminator itself is a trainable model.
-                            The generator(adv_loss, feature_matching_loss) is connected w/ the diffusion model,
-                            while the dis_loss is also connected w/ the diffusion model indirectly.
-                    '''
-                    loss, _, _ = self.discriminator.loss(decoded.unsqueeze(1),x.unsqueeze(1))
+                    
                     self.optim_dis.zero_grad()
-                    loss.backward()
+                    dis_loss.backward()
                     self.optim_dis.step()
-                    with self.warmup_schedule_dis.dampening():
-                        if self.warmup_schedule_dis.last_step + 1 >= self.MODEL_CFG['warmup_period']:
-                            self.lr_schedule_dis.step()
-                    info = {
-                        "discriminator/dis_loss" : loss
-                    }
-                
+                    
+                    info.update({"discriminator/dis_loss" : dis_loss})
                 else:
-                    loss, info = self.loss.loss_fn(decoded, x)
-                    _, adv_loss, feature_matching_loss = self.discriminator.loss(decoded.unsqueeze(1), x.unsqueeze(1))
-                    loss = loss + adv_loss + feature_matching_loss
-                    info.update(
-                        {
-                            'discriminator/adv_loss':adv_loss,
-                            "discriminator/feature_matching_loss": feature_matching_loss
-                        }
-                    )
+                    loss, info_loss_fn = self.loss.loss_fn(x, decoded)
+                    gen_loss = loss + adv_loss + feature_matching_loss
                     
                     self.optim_gen.zero_grad()
-                    loss.backward()
+                    gen_loss.backward()
                     self.optim_gen.step()
-                    with self.warmup_schedule_gen.dampening():
-                        if self.warmup_schedule_gen.last_step + 1 >= self.MODEL_CFG['warmup_period']:
-                            self.lr_schedule_gen.step()
-                            
+                    
+                    info.update(
+                        {
+                            **info_loss_fn,
+                            'discriminator/adv_loss': adv_loss,
+                            "discriminator/feature_matching_loss": feature_matching_loss,
+                            "generator_loss": gen_loss
+                        }
+                    )
+                    epochloss.append(gen_loss.item()) 
+                              
                 self.loss.discriminator_step += 1
-                        
-                # Timestamp Loss, LR
-                EPOCH_LOSS.append(loss.item())
-                wandb.log({"timestamp_loss": loss, 
-                           "lr": self.optim_gen.param_groups[0]['lr'],
-                           })
+                       
+                info.update({
+                    "lr/lr_dis": self.optim_dis.param_groups[0]['lr'],
+                    "lr/lr_gen": self.optim_gen.param_groups[0]['lr'],
+                    }
+                ) 
+               
                 wandb.log(info)
 
-            # Epoch Loss
-            avg_train_loss = sum(EPOCH_LOSS) / len(EPOCH_LOSS)
-            self.train_losses.append(avg_train_loss)
-            wandb.log({"train_loss": avg_train_loss})
-            print(f"Epoch {epoch+1}/{EPOCH}, Average Train Loss: {avg_train_loss:.4f}")
+            wandb.log({"Average Generator Loss": sum(epochloss) / len(epochloss)})
+            print(f"Epoch {epoch+1}/{EPOCH}, Average Generator Loss: {sum(epochloss) / len(epochloss)}")
 
-            # Evaluation & Sampling
+
             if (epoch + 1) % self.MODEL_CFG['evaluation_cycle'] == 0:
                 self.net.eval()
-                EVAL_LOSS = []
                 with torch.no_grad():
-                    STORE_SAMPLE_DIR = os.path.join(LOG_DIR, f"samples_epoch_{epoch+1}")
+                    EVAL_LOSS = []
+                    FAKE_DIR = os.path.join(LOG_DIR, f"generated_epoch_{epoch+1}")
+                    ORIG_DIR = os.path.join(LOG_DIR, f"original_epoch_{epoch+1}")
+                    os.makedirs(FAKE_DIR, exist_ok=True)
+                    os.makedirs(ORIG_DIR, exist_ok=True)
                     for idx,x in enumerate(self.test_loader):
-                        x = x[0].to(self.MODEL_CFG['device']) # list to TEnsor
+                        x = x[0].to(self.MODEL_CFG['device'])
                         latents = self.net.encode(x)
                         decoded = self.net.decode(latents)
+                        loss,_ = self.loss.loss_fn(x.unsqueeze(1), decoded.unsqueeze(1))
                         
-                        loss,_ = self.loss.loss_fn(decoded,x)
+                        self.generate_samples(decoded,output_dir=FAKE_DIR)
+                        self.generate_samples(x,output_dir=ORIG_DIR)
                         
-                        
-                        self.generate_samples(decoded,output_dir=STORE_SAMPLE_DIR)
-                        EVAL_LOSS.append(loss.item())
+                        EVAL_LOSS.append(loss.detach().cpu().item())
             
                 avg_eval_loss = sum(EVAL_LOSS) / len(EVAL_LOSS)
-                self.eval_losses.append(avg_eval_loss)
                 wandb.log({"eval_loss": avg_eval_loss})
                 print(f"Evaluation Loss: {avg_eval_loss:.4f}")
                 
@@ -177,23 +154,16 @@ class Trainer:
                 if avg_eval_loss < self.best_eval_loss:
                     self.best_eval_loss = avg_eval_loss
                     torch.save(self.net.state_dict(), os.path.join(LOG_DIR, 'best_model.pth'))
-                '''
-                STORE_SAMPLE_DIR = os.path.join(LOG_DIR, f"samples_epoch_{epoch+1}")
-                for idx in range(self.MODEL_CFG['num_samples']):
-                    self.generate_samples(
-                        num_samples=1,#self.MODEL_CFG['num_samples'],
-                        num_steps=self.MODEL_CFG['sampling_steps'][idx],
-                        output_dir=STORE_SAMPLE_DIR,
-                        idx=idx
-                    )
-
-                for i in range(self.MODEL_CFG['num_samples']):
-                    sample_path = os.path.join(STORE_SAMPLE_DIR, f"Sample_{i}.wav")
-                    wandb.log({f"Sample {i}": wandb.Audio(sample_path, sample_rate=self.FFT_CFG['sr'])})
-                '''
+                    print(f"Best model saved at {LOG_DIR}/best_model.pth")
+           
                 self.net.train()
 
-            
+            if ((epoch+1) % 10 == 0):
+                for name, module in self.net.named_modules():
+                    if hasattr(module, 'weight') and module.weight.grad is not None:
+                        wandb.log({f"gradients/{name}.weight": wandb.Histogram(module.weight.grad.cpu().numpy())})
+                    if hasattr(module, 'bias') and module.bias.grad is not None:
+                        wandb.log({f"gradients/{name}.bias": wandb.Histogram(module.bias.grad.cpu().numpy())})
 
 
 
@@ -219,12 +189,11 @@ class Trainer:
             output_dir: str,
         ) -> None:
         os.makedirs(output_dir, exist_ok=True)
-
         for i in range(x.size(0)):
             sample = x[i].cpu().numpy().flatten()
             filename = os.path.join(output_dir, f"Sample_{i}.wav")
             sf.write(filename, sample, self.FFT_CFG['sr'])
-            wandb.log({f"Sample {i}": wandb.Audio(sample, sample_rate=self.FFT_CFG['sr'])})
+            wandb.log({f"{output_dir}/Sample {i}": wandb.Audio(sample, sample_rate=self.FFT_CFG['sr'])})
 
      
 